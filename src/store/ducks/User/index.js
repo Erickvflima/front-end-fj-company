7a3968f0a32eb90ae271e290d24011887b2267a1
erick.lima <erick.lima@vileve.com.br>
2023-09-03 16:57:55 -0300

    insert signin dispatch

--- src/store/ducks/User/index.js
+++ src/store/ducks/User/index.js
@@ -1,12 +1,25 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-import { getList } from './service';
+import { getList, postSignin } from './service';
 
 export const listUser = createAsyncThunk(
   'user/list',
   async (_, { rejectWithValue }) => {
     try {
       const data = await getList();
+      return data;
+    } catch (error) {
+      const err = error;
+      return rejectWithValue(err.response?.data);
+    }
+  },
+);
+
+export const sendSignin = createAsyncThunk(
+  'user/signin',
+  async (cpf, { rejectWithValue }) => {
+    try {
+      const data = await postSignin(cpf);
       return data;
     } catch (error) {
       const err = error;
--- src/store/ducks/User/service.js
+++ src/store/ducks/User/service.js
@@ -5,3 +5,8 @@ export const getList = async () => {
   const response = await api.get(`/${patch}/list`);
   return response.data;
 };
+export const postSignin = async (cpf) => {
+  const response = await api.post(`/${patch}/signin?cpf=${cpf}`);
+  api.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
+  return response.data;
+};
